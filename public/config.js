40826d6b403469eed0d8bc45fa69deb075279fa7
Mario Reder <mario.reder@pm.me>
2025-09-30 20:09:39 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -17,7 +17,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_APP_DESCRIPTION": "DEX Perps trading",
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
-  "VITE_ENABLED_MENUS": "Leaderboard,Markets,Trading",
+  "VITE_ENABLED_MENUS": "Markets,Portfolio,Trading,Rewards",
   "VITE_CUSTOM_MENUS": "$futures, https://pump.fun/coin/G2Qx1RzC6HfUu9dRktwTeDi51nnNuVwAWyVciotHwGt1",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/cexpumpsup",
