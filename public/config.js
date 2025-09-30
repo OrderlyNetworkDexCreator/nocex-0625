5fe434f988fe6dcbbfa0ca442ec20127f90cf0b4
Mario Reder <mario.reder@pm.me>
2025-09-30 12:22:03 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/cexpump",
   "VITE_SEO_SITE_NAME": "CEXPump",
   "VITE_SEO_SITE_DESCRIPTION": "DEX Perps trading",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/nocex-0625/",
+  "VITE_SEO_SITE_URL": "https://futures.cexpump.com",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@cexpump",
